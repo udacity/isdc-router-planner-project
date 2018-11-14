60bfbbfd0809a4aac0d2adcb4b18aefe734931e4
mvirgo <mvirgs44@gmail.com>
2018-11-14 15:34:34 -0800

    feat: Update to latest version w starter code
    Updated for JS change to project to provide starter class and break out algo into numerous functions.\nAlso added some additional notes on what makes up the class

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-from helpers import load_map
+from helpers import load_map_40
 
 MAP_40_ANSWERS = [
     (5, 34, [5, 16, 37, 12, 34]),
     (5, 5,  [5]),
-    (8, 24, [8, 30, 16, 37, 12, 17, 10, 24])
+    (8, 24, [8, 14, 16, 37, 12, 17, 10, 24])
 ]
 
 def test(shortest_path_function):
-    map_40 = load_map('map-40.pickle')
+    map_40 = load_map_40()
     correct = 0
     for start, goal, answer_path in MAP_40_ANSWERS:
-        path = shortest_path_function(map_40, start, goal)
+        path = shortest_path_function(map_40, start, goal).path
         if path == answer_path:
             correct += 1
         else:
